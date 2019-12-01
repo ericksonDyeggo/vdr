d3562fbbd1ca79fcb48b8c0849a533a018a34878
Erickson Dyeggo <ericksondyeggo@gmail.com>
2019-12-01 11:22:06 -0300

    Fixes install

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # !/bin/sh
 if [ ! -d "$HOME/.vim" ]; then
-    echo "Installing MDR for the first time"
+    echo "Installing VDR for the first time"
+    git clone --depth=1 https://github.com/ericksonDyeggo/vdr.git "$HOME/.vim"
     ln -fs ~/.vim/vimrc ~/.vimrc
     vim -N "+set hidden" "+syntax on" +PlugInstall +qall
 else
